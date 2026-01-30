40bd6f9b6dfdefd212709e4147a158ba2f3527a0
Manish Tamang <yonzanmanish18@gmail.com>
2026-01-30 22:26:07 +0545

    learning-js

---
+++ Part-1/operation.js
@@ -0,0 +1,25 @@
+let score = 20;
+let bonus = 20;
+let finalScore = score + bonus;
+console.log(finalScore);
+
+let scorebonus = 20 + 20;
+console.log(scorebonus);
+
+let calories = 1200 / 40;
+console.log(calories);
+
+let totalCalories = 1200 * 40;
+console.log(totalCalories);
+
+let total = 40 - 40;
+console.log(total);
+
+let remainder = 51 % 5;
+console.log(remainder);
+
+let exponent = 2 ** 3;
+console.log(exponent);
+
+let myscore = 40;
+myscore += console.log(myscore);
