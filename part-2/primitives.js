a3d60dfbbbe647b4b3db340b43e70834611e146b
Manish Tamang <yonzanmanish18@gmail.com>
2026-01-31 21:40:09 +0545

    part-2

---
+++ part-2/primitives.js
@@ -0,0 +1,11 @@
+// Numbers
+let balance = 120;
+let anotherBalance = new Number(100);
+
+// console.log(balance);
+// console.log(anotherBalance.valueOf());
+
+// console.log(typeof balance);
+// console.log(typeof anotherBalance);
+
+//boolean
