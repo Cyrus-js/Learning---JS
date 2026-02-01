5267241f3d0c725aefc2d6665b1669e9ff463b09
Manish Tamang <yonzanmanish18@gmail.com>
2026-02-01 20:58:54 +0545

    nonprimitive-objects,array

---
+++ part-2/nonprimitives.js
@@ -0,0 +1,23 @@
+const username = {
+  "first Name": `Manish`,
+  isloggedin: true,
+};
+
+username.Name = "Pukulee";
+username.lastname = "Mero";
+
+// console.log(username["first Name"]);
+// console.log(username.lastname);
+// console.log(username);
+// console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+//Array
+
+let anotheruser = ["Manish", "Pukulee", 3, 4, true];
+// console.log(anotheruser[3]);
+
+let isvalue = "2acd";
+console.log(typeof Number(isvalue));
