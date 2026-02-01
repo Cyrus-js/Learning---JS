a5c37fed3d63a5cb790eb5aba7d1a3fdf87663b8
Manish Tamang <yonzanmanish18@gmail.com>
2026-02-01 15:27:24 +0545

    primitives

--- part-2/primitives.js
+++ part-2/primitives.js
@@ -9,3 +9,31 @@ let anotherBalance = new Number(100);
 // console.log(typeof anotherBalance);
 
 //boolean
+let isActive = true;
+let isreallyactive = new Boolean(true);
+// console.log(typeof isreallyactive);
+
+//Null and undefined
+let firstName = null;
+// console.log(firstName);
+
+//String
+let myString = "Pukuleee";
+let myString1 = "pretty";
+let userName = "Love";
+
+let oldgreet = myString + `` + `Beautiful`;
+// console.log(oldgreet);
+
+let greetMessage = `Helloo my ${userName} ${myString}!`;
+// console.log(greetMessage);
+
+let multiplication = `The value of 5*5 is ${5 * 5}`;
+// console.log(multiplication);
+
+//Symbol
+
+let sym = Symbol("hii");
+let sym1 = Symbol("hii");
+
+console.log(sym == sym1);
