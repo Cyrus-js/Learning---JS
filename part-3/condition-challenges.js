921b224dd677e1ee38b66e4b4f5a7c7df9dba615
Manish Tamang <yonzanmanish18@gmail.com>
2026-02-02 20:03:36 +0545

    condition-challenges in js

---
+++ part-3/condition-challenges.js
@@ -0,0 +1,58 @@
+// Checking if a number is greater than another number:
+let num1 = 10;
+let num2 = 5;
+
+/*
+console.log("I'm regular upper code");
+
+if (num1 < num2) {
+  console.log("num1 is greater than num2");
+} else {
+  console.log("num1 is not-greater than num2");
+}
+
+console.log("I'm regular bottom code");
+
+Checking if a string is equal to another string :
+*/
+
+/*
+
+ let male = "Manish";
+let female = "Pukule";
+
+if (male == female) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+ 
+*/
+
+let score = 44;
+
+if (typeof score === "number") {
+  //   console.log("It is a number");
+} else {
+  console.log("This is not a number");
+}
+
+// Checking is a boolean is true or false:
+let isAvailable = true;
+if (isAvailable) {
+  //   console.log("it is available");
+} else {
+  console.log("it is not available");
+}
+
+//Chrcking if a array is empty or not:
+
+let items = ["aeee"];
+
+console.log(items.length);
+
+if (items.length === 0) {
+  console.log("Array is empty");
+} else {
+  console.log("Array is not empty");
+}
