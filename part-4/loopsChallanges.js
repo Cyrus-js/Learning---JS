fc52fb4478f20248d2665a8f6a2f8349bfe20891
Manish Tamang <yonzanmanish18@gmail.com>
2026-02-06 23:14:16 +0545

    learning loops

--- part-4/loopsChallanges.js
+++ part-4/loopsChallanges.js
@@ -1,9 +1,19 @@
+// let sum = 0;
+// let i = 0;
+
+// while (i <= 5) {
+//   sum = sum + 1;
+//   sum += 1;
+//   i++;
+// }
+// console.log(sum);
+
 let sum = 0;
-let i = 0;
+let i = 1;
 
 while (i <= 5) {
-  sum = sum + 1;
-  sum += 1;
-  i++;
+  sum += i; // Add current number to sum
+  i++; // Move to next number
 }
-console.log(sum);
+
+console.log("Max sum 1+2+3+4+5 =", sum); // Output: 15
