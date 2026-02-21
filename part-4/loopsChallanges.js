85bb3fd699127ab0220f2fa37760cff9357db531
Manish Tamang <yonzanmanish18@gmail.com>
2026-02-21 18:05:34 +0545

    loops and loops and loops

--- part-4/loopsChallanges.js
+++ part-4/loopsChallanges.js
@@ -1,8 +1,11 @@
 // for loop //
 for (let i = 0; i < 5; i++) {
-  console.log("loop" + i);
+  // console.log("loop" + i);
 }
 
 for (let i = 5; i > 0; i--) {
   console.log("loop " + i);
+  if (i === 3) {
+    break;
+  }
 }
