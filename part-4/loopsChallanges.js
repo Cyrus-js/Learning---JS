55bef4b3740a9363e94cecd948544f62151f6c65
Manish Tamang <yonzanmanish18@gmail.com>
2026-02-25 23:30:21 +0545

    learning on web-console

--- part-4/loopsChallanges.js
+++ part-4/loopsChallanges.js
@@ -1,19 +1,30 @@
-// for loop //
-for (let i = 0; i < 5; i++) {
-  // console.log("loop" + i);
-}
+// // for loop //
+// for (let i = 0; i < 5; i++) {
+//   // console.log("loop" + i);
+// }
 
-for (let i = 5; i > 0; i--) {
-  // console.log("loop " + i);
-  if (i === 3) {
-    break;
+// for (let i = 5; i > 0; i--) {
+//   // console.log("loop " + i);
+//   if (i === 3) {
+//     break;
+//   }
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`cyrus` + i);
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//   console.log(`cyrus` + i);
+// }
+
+let teaCollection = [];
+let tea;
+
+do {
+  tea = prompt(`Enter your favourite tea(type "stop" to finish)`);
+
+  if (tea !== "stop") {
+    teaCollection.push(tea);
   }
-}
-
-for (let i = 0; i <= 10; i++) {
-  console.log(`cyrus` + i);
-}
-
-for (let i = 0; i <= 20; i++) {
-  console.log(`cyrus` + i);
-}
+} while (tea !== "stop");
