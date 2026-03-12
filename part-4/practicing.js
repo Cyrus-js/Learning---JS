0f50bed23f07adcd4cd9819592cd139bbb68a86d
Manish Tamang <yonzanmanish18@gmail.com>
2026-03-12 19:31:27 +0545

    for loop

--- part-4/practicing.js
+++ part-4/practicing.js
@@ -1,23 +1,26 @@
-// for loop //
-for (let i = 0; i < 5; i++) {
-  console.log("hellloo" + i);
-}
-let i = 1;
+// // for loop //
+// for (let i = 0; i < 5; i++) {
+//   console.log("hellloo" + i);
+// }
+// let i = 1;
 
-do {
-  console.log(i);
-  i++;
-} while (i <= 5);
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
 
-for (let h = 1; h <= 10; h++);
-console.log("helloo");
+// for (let h = 1; h <= 10; h++);
+// console.log("helloo");
 
-for (let j = 1; j <= 100; j++) {
-  console.log("Hello");
-}
+// for (let j = 1; j <= 100; j++) {
+//   console.log("Hello");
+// }
 
-// hello100.js
+// // hello100.js
 
-for (let j = 1; j <= 100; j++) {
+// for (let j = 1; j <= 100; j++) {
+//   console.log("Hello");
+// }
+for (let i = 1; i <= 10; i++) {
   console.log("Hello");
 }
