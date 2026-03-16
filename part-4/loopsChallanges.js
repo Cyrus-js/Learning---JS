5642dd711af4033d3e47d2a3c003c5545c20533e
Manish Tamang <yonzanmanish18@gmail.com>
2026-03-16 20:09:29 +0545

    grind

--- part-4/loopsChallanges.js
+++ part-4/loopsChallanges.js
@@ -60,15 +60,124 @@
 //   if (i === 3) break;
 //   console.log(i);
 // }
-let u = 1;
-{
-  if (((u = 3), u++));
-}
-console.log(u);
+// let u = 1;
+// {
+//   if (((u = 3), u++));
+// }
+// console.log(u);
 
+// let i = 1;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+let sum = 0;
 let i = 1;
+while (i <= 100) {
+  sum = sum + i;
+  i++;
+}
+// console.log(sum);
+
+let countdown = [];
+let j = 10;
+while (j > 0) {
+  countdown.push(j);
+  j--;
+}
+// console.log(countdown);
+
+let number = [];
+let k = 1;
+while (k <= 10) {
+  number.push(`the value is = ${k}`);
+  k++;
+}
+// console.log(number);
+
+let heros = ["manish", "tamang", "yonzan"];
+let power = 0;
+while (power < heros.length) {
+  // console.log(`heroes is ${heros[power]}`);
+  power = power + 1;
+}
+
+let arr = ["me", "she", "uss"];
+let usss = 0;
+while (usss < arr.length) {
+  // console.log(`gender = ${arr[usss]}`);
+  usss = usss + 1;
+}
+let add = 0;
+let a = 10;
+while (a >= 0) {
+  add = add + a;
+  a--;
+}
+// console.log(add);S
+
+let count = [];
+b = 5;
+while (b > 0) {
+  count.push(`the numbers are ${b}`);
+  b--;
+}
+// console.log(count);
+
+let names = ["a", "b", "c", "d"];
+let starting = 0;
+while (starting < names.length) {
+  // console.log(`ans is ${names[starting]}`);
+  starting = starting + 1;
+}
+
+let score = 10;
+do {
+  // console.log(`the score is ${score}`);
+  score--;
+} while (score >= 1);
+
+let word = 1;
+
+while (word <= 10) {
+  // console.log("helloooo");
+
+  word++;
+}
+
+let downs = [];
+let f = 5;
+while (f > 0) {
+  downs.push(f);
+  f--;
+}
+// console.log(downs);
+
+let teaCollection = [];
+let tea;
+
+// do {
+//   tea = prompt(`Enter your favourite tea,type "stop" to end`);
+//   if (tea !== "stop") {
+//     teaCollection.push(tea);
+//   }
+// } while (tea !== "stop");
+
+let start = 0;
+let initial = 1;
 
 do {
-  console.log(i);
-  i++;
-} while (i <= 5);
+  start += initial;
+  initial++;
+} while (initial <= 3);
+// console.log(start);
+
+let multipliedNumbers = [];
+let array = [2, 4, 6];
+for (let l = 0; l < array.length; l++) {
+  multiply = array[l] * 2;
+  multipliedNumbers.push(multiply);
+}
+// console.log(multipliedNumbers);
